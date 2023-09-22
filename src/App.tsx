8d5fc79f3ae93b28c052916a89da5f035f7f7113
luyannafm <luyannafm@gmail.com>
2023-09-22 13:22:36 -0300

    req 5 e 6 - done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Meals from './pages/Meals';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
+      <Route path="/meals" element={ <Meals /> } />
     </Routes>
   );
 }
