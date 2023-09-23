e27b0ddafdf4d3c088dce66f0f3ec5cc0aaa2d13
Sílvia Rodrigues de Morais <silviarmorais17@gmail.com>
2023-09-23 20:28:02 -0300

    Req 11 - ok

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,16 +1,47 @@
+import { useState } from 'react';
+import { fetchFirstLetter, fetchIngredient, fetchName } from '../services/FetchAPI';
+
 function SearchBar() {
+  const [value, setValue] = useState('');
+  const [text, setText] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const handleSearch = async (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (value === 'first letter' && text.length > 1) {
+      window.alert('Your search must have only 1 (one) character');
+    } if (value === 'first letter') {
+      const data = await fetchFirstLetter(text);
+      return data;
+    }
+    if (value === 'ingredient') {
+      const data = await fetchIngredient(text);
+      return data;
+    }
+    if (value === 'name') {
+      const data = await fetchName(text);
+      return data;
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={ handleSearch }>
       <input
         type="text"
         placeholder="Search"
         data-testid="search-input"
+        onChange={ (event) => setText(event.target.value) }
       />
       <input
         type="radio"
         name="radio"
         id="ingredient"
         value="ingredient"
+        onChange={ handleChange }
         data-testid="ingredient-search-radio"
       />
       <label htmlFor="ingredient">Ingredient</label>
@@ -20,6 +51,7 @@ function SearchBar() {
         name="radio"
         id="name"
         value="name"
+        onChange={ handleChange }
         data-testid="name-search-radio"
       />
       <label htmlFor="name">Name</label>
@@ -29,6 +61,7 @@ function SearchBar() {
         name="radio"
         id="first letter"
         value="first letter"
+        onChange={ handleChange }
         data-testid="first-letter-search-radio"
       />
       <label htmlFor="first letter">First letter</label>
---
+++ src/services/FetchAPI.tsx
@@ -0,0 +1,17 @@
+export const fetchIngredient = async (ingredient: string) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+  const data = await response.json();
+  console.log(data);
+};
+
+export const fetchName = async (name: string) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+  const data = await response.json();
+  return data;
+};
+
+export const fetchFirstLetter = async (letter: string) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`);
+  const data = await response.json();
+  return data;
+};
