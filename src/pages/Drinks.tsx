066f70dc665ac99a1a0f238cdd9d8ef53fa03d77
Pablo Andrade Tunes <pabloiae123@gmail.com>
2023-09-25 03:57:47 -0300

    Requirements 17 and 18

--- src/pages/Drinks.tsx
+++ src/pages/Drinks.tsx
@@ -1,4 +1,5 @@
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Drinks() {
   return (
@@ -6,6 +7,7 @@ function Drinks() {
       <h1>
         <Header pageTitle="Drinks" showSearchIcon />
       </h1>
+      <Footer />
     </div>
   );
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,5 @@
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Profile() {
   return (
@@ -6,6 +7,7 @@ function Profile() {
       <h1>
         <Header pageTitle="Profile" showSearchIcon={ false } />
       </h1>
+      <Footer />
     </div>
   );
 }
