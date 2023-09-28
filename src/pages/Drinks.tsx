0ae4e466657d14ac95eebaaa92948796cbeccd0f
Bruno Medeiros <brunoml95@hotmail.com>
2023-09-28 10:28:19 -0300

    req 19 e 20 sem teste

--- src/pages/Drinks.tsx
+++ src/pages/Drinks.tsx
@@ -1,12 +1,80 @@
+import { useEffect, useState, useContext } from 'react';
+import { fetchNameDrinks, fetchCategoryDrinks } from '../services/FetchAPIDrinks';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Recipes from '../components/Recipes';
+import GlobalContext from '../context/GlobalContext';
+
+type RecipeDrinks = {
+  idDrink: string;
+  strDrink: string;
+  strDrinkThumb: string;
+};
+
+type Category = {
+  strCategory: string;
+};
 
 function Drinks() {
+  const [recipesDrinks, setRecipesDrinks] = useState<RecipeDrinks[]>([]);
+  const { dataApi } = useContext(GlobalContext);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const loadRecipes = async () => {
+      try {
+        const response = await fetchNameDrinks('');
+        if (response.drinks && response.drinks.length > 0) {
+          setRecipesDrinks(response.drinks.slice(0, 12));
+        }
+      } catch (error) {
+        console.error('Erro ao carregar as receitas:', error);
+      }
+    };
+    loadRecipes();
+  }, [dataApi]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetchCategoryDrinks();
+        if (response.drinks && response.drinks.length > 0) {
+          setCategories(response.drinks.slice(0, 5));
+        }
+      } catch (error) {
+        console.error('Erro ao carregar categorias:', error);
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <div>
-      <h1>
-        <Header pageTitle="Drinks" showSearchIcon />
-      </h1>
+      <Header pageTitle="Drinks" showSearchIcon />
+      <div className="category-buttons">
+        {categories.map((category, index) => (
+          <button
+            key={ index }
+            data-testid={ `${category.strCategory}-category-filter` }
+          >
+            {category.strCategory}
+          </button>
+        ))}
+      </div>
+      {dataApi.drinks && dataApi.drinks.length === 0
+        && recipesDrinks.map((recipe, index) => (
+          <div className="recipe-card" key={ recipe.idDrink }>
+            <div data-testid={ `${index}-recipe-card` }>
+              <img
+                src={ recipe.strDrinkThumb }
+                alt={ recipe.strDrink }
+                data-testid={ `${index}-card-img` }
+              />
+              <p data-testid={ `${index}-card-name` }>{recipe.strDrink}</p>
+            </div>
+          </div>
+        ))}
+      {dataApi.drinks && dataApi.drinks.length !== 0 && <Recipes />}
       <Footer />
     </div>
   );
--- src/tests/Login.test.tsx
+++ src/tests/Login.test.tsx
@@ -1,16 +1,56 @@
-import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
-import App from '../App';
-import { renderWithRouter } from '../helps/renderWithRouter';
+import { BrowserRouter } from 'react-router-dom';
+import { fireEvent, screen, render, waitFor } from '@testing-library/react';
+import Login from '../pages/Login';
+
+const EMAIL_INPUT = 'email-input';
+const PASSWORD_INPUT = 'password-input';
+const LOGIN_SUBMIT = 'login-submit-btn';
 
 test('O botão deve estar desativado se o email e a senha forem inválidos', async () => {
-  renderWithRouter(
-    <App />,
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>,
+  );
+  const emailInput = screen.getByTestId(EMAIL_INPUT);
+  const passwordInput = screen.getByTestId(PASSWORD_INPUT);
+  const loginSubmitButton = screen.getByTestId(LOGIN_SUBMIT);
+
+  expect(loginSubmitButton).toBeDisabled();
+
+  fireEvent.change(emailInput, { target: { value: 'safasfasdf' } });
+  fireEvent.change(passwordInput, { target: { value: 'teste' } });
+
+  expect(loginSubmitButton).toBeDisabled();
+});
+
+test('O botão deve estar ativado se o email e a senha forem válidos', () => {
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>,
   );
 
-  const emailInput = screen.getByTestId('email-input');
-  const passwordInput = screen.getByTestId('password-input');
-  const loginSubmitButton = screen.getByTestId('login-submit-btn');
+  const emailInput = screen.getByTestId(EMAIL_INPUT);
+  const passwordInput = screen.getByTestId(PASSWORD_INPUT);
+  const loginSubmitButton = screen.getByTestId(LOGIN_SUBMIT);
+
+  fireEvent.change(emailInput, { target: { value: 'email@exemplo.com' } });
+  fireEvent.change(passwordInput, { target: { value: 'senhaSegura' } });
+
+  expect(loginSubmitButton).toBeEnabled();
+});
+
+test('O botão deve direcionar para rota /meals', async () => {
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>,
+  );
+
+  const emailInput = screen.getByTestId(EMAIL_INPUT);
+  const passwordInput = screen.getByTestId(PASSWORD_INPUT);
+  const loginSubmitButton = screen.getByTestId(LOGIN_SUBMIT);
 
   expect(loginSubmitButton).toBeDisabled();
 
@@ -21,20 +61,7 @@ test('O botão deve estar desativado se o email e a senha forem inválidos', asy
 
   fireEvent.click(loginSubmitButton);
 
-  await screen.findByText('Meals');
-});
-
-test('O botão deve estar ativado se o email e a senha forem válidos', () => {
-  renderWithRouter(
-    <App />,
-  );
-
-  const emailInput = screen.getByTestId('email-input');
-  const passwordInput = screen.getByTestId('password-input');
-  const loginSubmitButton = screen.getByTestId('login-submit-btn');
-
-  fireEvent.change(emailInput, { target: { value: 'email@exemplo.com' } });
-  fireEvent.change(passwordInput, { target: { value: 'senhaSegura' } });
-
-  expect(loginSubmitButton).toBeEnabled();
+  await waitFor(() => {
+    expect(window.location.pathname).toBe('/meals');
+  });
 });
